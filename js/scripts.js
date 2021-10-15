7bd89de84a060f461115e4677fbf73b71b116685
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-15 14:48:55 -0500

    Complete html markup and styles for navbar and menu button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 /*-----Javascript for js-food-website Name */
 $(document).ready(function () {
+	/*=============================================
+               navbar and menu
+   ================================================*/
+   document.querySelector('.menu').addEventListener('click', () => {
+		document.querySelectorAll('.target').forEach(item => {
+			item.classList.toggle('change');
+		});
+	});
+	/*=============================================
+               landing page slider
+   ================================================*/
 	const icons = document.querySelectorAll('.section-one-icons i');
 	let i = 1;
 	setInterval(() => {
@@ -10,7 +21,6 @@ $(document).ready(function () {
 		if (i > icons.length) {
 			icons[0].classList.add('change');
 			i = 1;
-         
 		} else {
 			icon.nextElementSibling.classList.add('change');
 		}
