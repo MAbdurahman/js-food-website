0b6937ea41968eb692ff091c88e85879c175b08e
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-15 03:55:00 -0500

    Complete a portion of the landing page and its slide show

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-/*-----Javascript for Project Name */
-$(document).ready(function () {});
+/*-----Javascript for js-food-website Name */
+$(document).ready(function () {
+	const icons = document.querySelectorAll('.section-one-icons i');
+	let i = 1;
+	setInterval(() => {
+		i++;
+		const icon = document.querySelector('.section-one-icons .change');
+		icon.classList.remove('change');
+
+		if (i > icons.length) {
+			icons[0].classList.add('change');
+			i = 1;
+         
+		} else {
+			icon.nextElementSibling.classList.add('change');
+		}
+	}, 3000);
+});
